314385844fca683a94d395d0cd136bd25ebe32b8
afoninsky <vkfont@yandex.ru>
2017-01-14 09:37:31 +0300

    wip

--- exercises/introduction/solution/solution.js
+++ exercises/introduction/solution/solution.js
@@ -1,12 +1,7 @@
-var Hapi = require('hapi');
-var path = require('path');
-
-var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: {
-        file: path.join(__dirname, '/index.html')
-    }
-});
-server.start();
+const bishop = require('bishop')()
+bishop.add('role: math, cmd: sum', message => {
+  return {
+    result: message.left + message.right
+  }
+})
+module.exports = bishop
